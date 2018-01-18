1d2fb8a0ffdbc674bf8f98b526c296b85b9f3f79
Thales <thales.pessoa@gmail.com>
2018-01-18 20:31:35 -0200

    removing dagger-android lib

--- app/src/main/java/com/pixformance/themovie/app/ApplicationController.java
+++ app/src/main/java/com/pixformance/themovie/app/ApplicationController.java
@@ -1,19 +1,11 @@
 package com.pixformance.themovie.app;
 
 import android.app.Application;
-import android.app.Fragment;
-
-import javax.inject.Inject;
-
-import dagger.android.DispatchingAndroidInjector;
 
 /**
  * Main app class. This setups applications modules and start routines
  */
 public class ApplicationController extends Application {
-
-    @Inject
-    DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
 
     private ApplicationComponent applicationComponent;
 
